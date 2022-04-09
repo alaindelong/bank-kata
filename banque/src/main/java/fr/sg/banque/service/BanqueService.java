fb49a0ecba7d7c03ec21fa54b19b1fd20d6ee9ef
alaindelong <alaindelong40@gmail.com>
2022-04-10 01:18:29 +0200

    ajout signature methode

--- banque/src/main/java/fr/sg/banque/service/BanqueService.java
+++ banque/src/main/java/fr/sg/banque/service/BanqueService.java
@@ -1,8 +1,13 @@
 package fr.sg.banque.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import fr.sg.banque.model.Operation;
+
 public interface BanqueService {
 
-	public void verser();
-	public void retirer();
-	public void consulter();
+	public void verser(String numeroCompte, BigDecimal montant);
+	public void retirer(String numeroCompte, BigDecimal montant);
+	public List<Operation> consulter(String numeroCompte);
 }
--- banque/src/main/java/fr/sg/banque/service/BanqueServiceImpl.java
+++ banque/src/main/java/fr/sg/banque/service/BanqueServiceImpl.java
@@ -1,23 +1,29 @@
 package fr.sg.banque.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import fr.sg.banque.model.Operation;
+
 public class BanqueServiceImpl implements BanqueService {
 
 	@Override
-	public void verser() {
+	public void verser(String numeroCompte, BigDecimal montant) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void retirer() {
+	public void retirer(String numeroCompte, BigDecimal montant) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void consulter() {
+	public List<Operation> consulter(String numeroCompte) {
 		// TODO Auto-generated method stub
-		
+		return null;
 	}
+
 
 }
